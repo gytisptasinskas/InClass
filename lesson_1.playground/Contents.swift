e2779dafe08b31969dc847096cb4d5c61e3d0b29
Gytis Ptašinskas <51530980+gytisptasinskas@users.noreply.github.com>
2023-10-04 15:24:59 +0300

    Updated lesson 1
    Added with new information from Oct 4th

--- lesson_1.playground/Contents.swift
+++ lesson_1.playground/Contents.swift
@@ -64,3 +64,57 @@ if let string = optionalString {
     }
 }
 
+//MARK: -Tuples
+var myNameIs: (String, String) = ("A", "M")
+
+var myBossIs: (String, String, Int) = ("Jake", "Doe", 44)
+print("my boss name is \(myBossIs.0) \(myBossIs.1) and he is \(myBossIs.2) years old")
+
+
+//MARK: -Operatos
+
+var a = 10
+a = a + 1
+a = a - 1
+a = a * 1
+a = a / 2
+
+var b = 10
+b += 1
+b /= 2
+
+let e = 9
+let f = 4
+
+var g = e % f
+print("g is", g)
+
+
+a == b
+a != b
+a > b
+a < b
+a >= b
+a <= b
+
+
+//MARK: -Logical operators
+
+let latestXcodeVersion = false
+let latestOSVerion = true
+
+if !latestOSVerion {
+    if latestXcodeVersion {
+        print("My latest verion")
+    }
+}
+
+if latestXcodeVersion && latestOSVerion {
+    print("My both version are up todate")
+}
+
+if latestXcodeVersion || latestOSVerion {
+    print("latestXcodeVersion", latestXcodeVersion)
+    print("latestOSVerion", latestOSVerion)
+    print("one of my version are up todate")
+}
